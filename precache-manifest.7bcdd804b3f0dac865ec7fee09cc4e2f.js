71bc65bf9d3b3b764ee163c05553bb16af14155b
Awu Chen <awu.mxr@gmail.com>
2025-08-08 10:55:40 +0900

    Updates

---
+++ precache-manifest.7bcdd804b3f0dac865ec7fee09cc4e2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1bf418c0028e24a17ef81e51b16a39",
+    "url": "/GreifNet/index.html"
+  },
+  {
+    "revision": "e03ff4a3b234e8fdd7f3",
+    "url": "/GreifNet/static/css/main.01b6fc35.chunk.css"
+  },
+  {
+    "revision": "c99a95615be67c63de8b",
+    "url": "/GreifNet/static/js/2.1706f975.chunk.js"
+  },
+  {
+    "revision": "7b36c4a4b00d4c1eb1f21a15ccb34795",
+    "url": "/GreifNet/static/js/2.1706f975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e03ff4a3b234e8fdd7f3",
+    "url": "/GreifNet/static/js/main.6eaeb058.chunk.js"
+  },
+  {
+    "revision": "99dc48f28078f78a4eab",
+    "url": "/GreifNet/static/js/runtime-main.7c1d5643.js"
+  }
+]);
